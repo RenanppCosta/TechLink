422fb955e3cc4db075ff6d3a61ecd00fcce8578c
Pedro Sena <phasena@gmail.com>
2025-06-29 18:29:31 -0300

    Add filter and sorting to professor search
    Enhanced the professor search page with a visual filter bar and sorting options (A-Z, Z-A, most popular). Updated the view to support filtering by name, surname, or area, and to order results based on user selection.

--- techlink/views/professor_search_view.py
+++ techlink/views/professor_search_view.py
@@ -8,8 +8,21 @@ class ProfessorSearchView(ListView):
     context_object_name = 'professores'
 
     def get_queryset(self):
-        query = self.request.GET.get('q', '')
-        return PerfilProfessor.objects.filter(
-            Q(usuario__nome__icontains=query) |
-            Q(temas__nome__icontains=query)
-        ).distinct()
+        query = self.request.GET.get('q', '').strip()
+        ordenar = self.request.GET.get('ordenar')
+
+        qs = PerfilProfessor.objects.all()
+
+        if query:
+            qs = qs.filter(
+                Q(usuario__nome__icontains=query) |
+                Q(usuario__sobrenome__icontains=query) |
+                Q(temas__nome__icontains=query)
+            ).distinct()
+
+        if ordenar == 'asc':
+            qs = qs.order_by('usuario__nome')
+        elif ordenar == 'desc':
+            qs = qs.order_by('-usuario__nome')
+
+        return qs
